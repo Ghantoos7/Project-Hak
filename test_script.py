0ea3a594612b4ea7a92221f02ba7c7bb5ebd858a
Ghantoos <ghnatios1@gmail.com>
2023-03-21 10:07:09 +0200

    added functions for href retrieval

--- test_script.py
+++ test_script.py
@@ -1,4 +1,5 @@
-
+import requests
+import re
 
 def openFile(file_name):
 
@@ -16,3 +17,22 @@ def writeToFile(valid_list,file_name):
         for valid in valid_list:
             file.write(valid + "\n")
 # method to write to a file and store each element in the list in a line
+
+
+# This function takes a URL as input and sends an HTTP GET request to the URL using the requests library.
+# It then returns the response object containing the server's response to the request.
+def openUrl(url):
+    request = requests.get("https://" + url)
+    return request
+
+# This function takes a requests object as input and returns the text content of the response as a string.
+def getHtml(request : requests):
+    html = request.text
+    return html
+
+# This function takes an HTML string as input and uses a regular expression to find all href links in the HTML.
+# It then returns a list of all href links found in the HTML.
+def getHrefList(html):
+    pattern = r'<(?:a|link|image|audio|video) href="(.+?)">'
+    href_list = re.findall(pattern,html)
+    return href_list
